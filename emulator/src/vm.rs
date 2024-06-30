d1a562a3cbab098a6981fdf30e54eac38136eee1
Sxmourai <samourai_x@outlook.fr>
2024-06-30 08:05:51 +0200

    feature: instruction execution !

--- emulator/src/vm.rs
+++ emulator/src/vm.rs
@@ -1,6 +1,8 @@
-use std::{cell::OnceCell, collections::LinkedList};
+use std::{any::type_name, cell::OnceCell, collections::LinkedList};
 
 use bit_field::BitField;
+
+use crate::instructions;
 
 struct VM {
     program: Vec<u8>,
@@ -63,133 +65,163 @@ bitfield::bitfield! {
     pub rs2, _: 24, 20;
     pub hi_imm, _: 31, 25;
 }
+#[derive(Debug, Clone, Copy)]
 pub enum InstructionFormat {
     R,I,S,B,U,J,
 }
 
-const fn _mask(opcode: u32, fun3: u32, fun7: u32) -> u32 {
-    opcode | fun3 << 12 | fun7 << 25
+const fn _mask(opcode: u32, fun3: u32, fun7: u32) -> InstructionMask {
+    InstructionMask(opcode | fun3 << 12 | fun7 << 25)
 }
 
 // Big thanks to https://www.eg.bucknell.edu/~csci206/riscv-converter/Annotated_RISCV_Card.pdf
-const INSTRUCTIONS_MASKS: [(&'static str, InstructionFormat, u32); 63] = [
-    ("lb",      InstructionFormat::I, _mask(0b0000011, 0b000, 0b0)), // ! Do we need to set fun7 ?
-    ("lh",      InstructionFormat::I, _mask(0b0000011, 0b001, 0b0)),
-    ("lw",      InstructionFormat::I, _mask(0b0000011, 0b010, 0b0)),
-    ("ld",      InstructionFormat::I, _mask(0b0000011, 0b011, 0b0)),
-    ("lbu",     InstructionFormat::I, _mask(0b0000011, 0b100, 0b0)),
-    ("lhu",     InstructionFormat::I, _mask(0b0000011, 0b110, 0b0)),
-    ("lwu",     InstructionFormat::I, _mask(0b0000011, 0b111, 0b0)),
+const INSTRUCTIONS_MASKS: [(&'static str, InstructionFormat, InstructionMask, fn(Instruction)); 63] = [
+    ("lb",      InstructionFormat::I, _mask(0b0000011, 0b000, 0b0), empty_fun), // ! Do we need to set fun7 ?
+    ("lh",      InstructionFormat::I, _mask(0b0000011, 0b001, 0b0), empty_fun),
+    ("lw",      InstructionFormat::I, _mask(0b0000011, 0b010, 0b0), empty_fun),
+    ("ld",      InstructionFormat::I, _mask(0b0000011, 0b011, 0b0), empty_fun),
+    ("lbu",     InstructionFormat::I, _mask(0b0000011, 0b100, 0b0), empty_fun),
+    ("lhu",     InstructionFormat::I, _mask(0b0000011, 0b110, 0b0), empty_fun),
+    ("lwu",     InstructionFormat::I, _mask(0b0000011, 0b111, 0b0), empty_fun),
     
-    ("fence",   InstructionFormat::I, _mask(0b0001111, 0b000, 0b0)),
-    ("fence.i", InstructionFormat::I, _mask(0b0001111, 0b001, 0b0)),
+    ("fence",   InstructionFormat::I, _mask(0b0001111, 0b000, 0b0), empty_fun),
+    ("fence.i", InstructionFormat::I, _mask(0b0001111, 0b001, 0b0), empty_fun),
     
-    ("addi",    InstructionFormat::I, _mask(0b0010011, 0b000, 0b0)),
-    ("slli",    InstructionFormat::I, _mask(0b0010011, 0b001, 0b0)), // Has funct7 ??
-    ("slti",    InstructionFormat::I, _mask(0b0010011, 0b010, 0b0)),
-    ("sltiu",   InstructionFormat::I, _mask(0b0010011, 0b011, 0b0)),
-    ("xori",    InstructionFormat::I, _mask(0b0010011, 0b100, 0b0)),
-    ("srli",    InstructionFormat::I, _mask(0b0010011, 0b101, 0b0000000)),
-    ("srai",    InstructionFormat::I, _mask(0b0010011, 0b101, 0b0100000)),
-    ("ori",     InstructionFormat::I, _mask(0b0010011, 0b110, 0b0)),
-    ("andi",    InstructionFormat::I, _mask(0b0010011, 0b111, 0b0)),
+    ("addi",    InstructionFormat::I, _mask(0b0010011, 0b000, 0b0), empty_fun),
+    ("slli",    InstructionFormat::I, _mask(0b0010011, 0b001, 0b0), empty_fun), // Has funct7 ??
+    ("slti",    InstructionFormat::I, _mask(0b0010011, 0b010, 0b0), empty_fun),
+    ("sltiu",   InstructionFormat::I, _mask(0b0010011, 0b011, 0b0), empty_fun),
+    ("xori",    InstructionFormat::I, _mask(0b0010011, 0b100, 0b0), empty_fun),
+    ("srli",    InstructionFormat::I, _mask(0b0010011, 0b101, 0b0000000), empty_fun),
+    ("srai",    InstructionFormat::I, _mask(0b0010011, 0b101, 0b0100000), empty_fun),
+    ("ori",     InstructionFormat::I, _mask(0b0010011, 0b110, 0b0), empty_fun),
+    ("andi",    InstructionFormat::I, _mask(0b0010011, 0b111, 0b0), empty_fun),
     
-    ("auipc",   InstructionFormat::U, _mask(0b0010111, 0b000, 0b0)),
+    ("auipc",   InstructionFormat::U, _mask(0b0010111, 0b000, 0b0), empty_fun),
     
-    ("addiw",   InstructionFormat::I, _mask(0b0011011, 0b000, 0b0)),
-    ("slliw",   InstructionFormat::I, _mask(0b0011011, 0b001, 0b0000000)),
-    ("srliw",   InstructionFormat::I, _mask(0b0011011, 0b101, 0b0000000)),
-    ("sraiw",   InstructionFormat::I, _mask(0b0011011, 0b101, 0b0100000)),
+    ("addiw",   InstructionFormat::I, _mask(0b0011011, 0b000, 0b0), empty_fun),
+    ("slliw",   InstructionFormat::I, _mask(0b0011011, 0b001, 0b0000000), empty_fun),
+    ("srliw",   InstructionFormat::I, _mask(0b0011011, 0b101, 0b0000000), empty_fun),
+    ("sraiw",   InstructionFormat::I, _mask(0b0011011, 0b101, 0b0100000), empty_fun),
 
-    ("sb",   InstructionFormat::S, _mask(0b0100011, 0b000, 0b0)),
-    ("sh",   InstructionFormat::S, _mask(0b0100011, 0b001, 0b0)),
-    ("sw",   InstructionFormat::S, _mask(0b0100011, 0b010, 0b0)),
-    ("sd",   InstructionFormat::S, _mask(0b0100011, 0b011, 0b0)),
+    ("sb",   InstructionFormat::S, _mask(0b0100011, 0b000, 0b0), empty_fun),
+    ("sh",   InstructionFormat::S, _mask(0b0100011, 0b001, 0b0), empty_fun),
+    ("sw",   InstructionFormat::S, _mask(0b0100011, 0b010, 0b0), empty_fun),
+    ("sd",   InstructionFormat::S, _mask(0b0100011, 0b011, 0b0), empty_fun),
 
-    ("sb",   InstructionFormat::R, _mask(0b0100011, 0b000, 0b0)),
-    ("sh",   InstructionFormat::R, _mask(0b0100011, 0b001, 0b0)),
-    ("sw",   InstructionFormat::R, _mask(0b0100011, 0b010, 0b0)),
-    ("sd",   InstructionFormat::R, _mask(0b0100011, 0b011, 0b0)),
-    ("add",  InstructionFormat::R, _mask(0b0110011, 0b000, 0b0000000)),
-    ("sub",  InstructionFormat::R, _mask(0b0110011, 0b000, 0b0100000)),
-    ("sil",  InstructionFormat::R, _mask(0b0110011, 0b001, 0b0000000)),
-    ("slt",  InstructionFormat::R, _mask(0b0110011, 0b010, 0b0000000)),
-    ("sltu", InstructionFormat::R, _mask(0b0110011, 0b011, 0b0000000)),
-    ("xor",  InstructionFormat::R, _mask(0b0110011, 0b100, 0b0000000)),
-    ("srl",  InstructionFormat::R, _mask(0b0110011, 0b101, 0b0000000)),
-    ("sra",  InstructionFormat::R, _mask(0b0110011, 0b101, 0b0100000)),
-    ("or",   InstructionFormat::R, _mask(0b0110011, 0b110, 0b0000000)),
-    ("and",  InstructionFormat::R, _mask(0b0110011, 0b111, 0b0000000)),
+    ("sb",   InstructionFormat::R, _mask(0b0100011, 0b000, 0b0), empty_fun),
+    ("sh",   InstructionFormat::R, _mask(0b0100011, 0b001, 0b0), empty_fun),
+    ("sw",   InstructionFormat::R, _mask(0b0100011, 0b010, 0b0), empty_fun),
+    ("sd",   InstructionFormat::R, _mask(0b0100011, 0b011, 0b0), empty_fun),
+    ("add",  InstructionFormat::R, _mask(0b0110011, 0b000, 0b0000000), add),
+    ("sub",  InstructionFormat::R, _mask(0b0110011, 0b000, 0b0100000), empty_fun),
+    ("sil",  InstructionFormat::R, _mask(0b0110011, 0b001, 0b0000000), empty_fun),
+    ("slt",  InstructionFormat::R, _mask(0b0110011, 0b010, 0b0000000), empty_fun),
+    ("sltu", InstructionFormat::R, _mask(0b0110011, 0b011, 0b0000000), empty_fun),
+    ("xor",  InstructionFormat::R, _mask(0b0110011, 0b100, 0b0000000), empty_fun),
+    ("srl",  InstructionFormat::R, _mask(0b0110011, 0b101, 0b0000000), empty_fun),
+    ("sra",  InstructionFormat::R, _mask(0b0110011, 0b101, 0b0100000), empty_fun),
+    ("or",   InstructionFormat::R, _mask(0b0110011, 0b110, 0b0000000), empty_fun),
+    ("and",  InstructionFormat::R, _mask(0b0110011, 0b111, 0b0000000), empty_fun),
 
-    ("lui",  InstructionFormat::U, _mask(0b0110111, 0b0, 0b0)),
+    ("lui",  InstructionFormat::U, _mask(0b0110111, 0b0, 0b0), empty_fun),
     
-    ("addw", InstructionFormat::R, _mask(0b0111011, 0b000, 0b0000000)),
-    ("subw", InstructionFormat::R, _mask(0b0111011, 0b000, 0b0100000)),
-    ("sllw", InstructionFormat::R, _mask(0b0111011, 0b001, 0b0000000)),
-    ("srllw",InstructionFormat::R, _mask(0b0111011, 0b101, 0b0000000)),
-    ("sraw", InstructionFormat::R, _mask(0b0111011, 0b101, 0b0100000)),
+    ("addw", InstructionFormat::R, _mask(0b0111011, 0b000, 0b0000000), empty_fun),
+    ("subw", InstructionFormat::R, _mask(0b0111011, 0b000, 0b0100000), empty_fun),
+    ("sllw", InstructionFormat::R, _mask(0b0111011, 0b001, 0b0000000), empty_fun),
+    ("srllw",InstructionFormat::R, _mask(0b0111011, 0b101, 0b0000000), empty_fun),
+    ("sraw", InstructionFormat::R, _mask(0b0111011, 0b101, 0b0100000), empty_fun),
 
-    ("beq",  InstructionFormat::B, _mask(0b1100011, 0b000, 0b0)),
-    ("bne",  InstructionFormat::B, _mask(0b1100011, 0b001, 0b0)),
-    ("bit",  InstructionFormat::B, _mask(0b1100011, 0b100, 0b0)),
-    ("bge",  InstructionFormat::B, _mask(0b1100011, 0b101, 0b0)),
-    ("bitu", InstructionFormat::B, _mask(0b1100011, 0b110, 0b0)),
-    ("bgeu", InstructionFormat::B, _mask(0b1100011, 0b111, 0b0)),
+    ("beq",  InstructionFormat::B, _mask(0b1100011, 0b000, 0b0), empty_fun),
+    ("bne",  InstructionFormat::B, _mask(0b1100011, 0b001, 0b0), empty_fun),
+    ("bit",  InstructionFormat::B, _mask(0b1100011, 0b100, 0b0), empty_fun),
+    ("bge",  InstructionFormat::B, _mask(0b1100011, 0b101, 0b0), empty_fun),
+    ("bitu", InstructionFormat::B, _mask(0b1100011, 0b110, 0b0), empty_fun),
+    ("bgeu", InstructionFormat::B, _mask(0b1100011, 0b111, 0b0), empty_fun),
 
-    ("jalr", InstructionFormat::I, _mask(0b1100111, 0b111, 0b0)),
+    ("jalr", InstructionFormat::I, _mask(0b1100111, 0b111, 0b0), empty_fun),
     
-    ("jal",  InstructionFormat::J, _mask(0b1101111, 0b0, 0b0)),
+    ("jal",  InstructionFormat::J, _mask(0b1101111, 0b0, 0b0), empty_fun),
 
-    ("ecall",  InstructionFormat::I, _mask(0b1110011, 0b0, 0b0)), // Immediates (fun7)
-    ("ebreak", InstructionFormat::I, _mask(0b1110011, 0b0, 0b1)),
+    ("ecall",  InstructionFormat::I, _mask(0b1110011, 0b0, 0b0), empty_fun), // Immediates (fun7)
+    ("ebreak", InstructionFormat::I, _mask(0b1110011, 0b0, 0b1), empty_fun),
 
-    ("CSRRW",  InstructionFormat::I, _mask(0b1110011, 0b001, 0b0)),
-    ("CSRRS",  InstructionFormat::I, _mask(0b1110011, 0b010, 0b0)),
-    ("CSRRC",  InstructionFormat::I, _mask(0b1110011, 0b011, 0b0)),
-    ("CSRRWI", InstructionFormat::I, _mask(0b1110011, 0b101, 0b0)),
-    ("CSRRSI", InstructionFormat::I, _mask(0b1110011, 0b110, 0b0)),
-    ("CSRRCI", InstructionFormat::I, _mask(0b1110011, 0b111, 0b0)),
+    ("CSRRW",  InstructionFormat::I, _mask(0b1110011, 0b001, 0b0), empty_fun),
+    ("CSRRS",  InstructionFormat::I, _mask(0b1110011, 0b010, 0b0), empty_fun),
+    ("CSRRC",  InstructionFormat::I, _mask(0b1110011, 0b011, 0b0), empty_fun),
+    ("CSRRWI", InstructionFormat::I, _mask(0b1110011, 0b101, 0b0), empty_fun),
+    ("CSRRSI", InstructionFormat::I, _mask(0b1110011, 0b110, 0b0), empty_fun),
+    ("CSRRCI", InstructionFormat::I, _mask(0b1110011, 0b111, 0b0), empty_fun),
 ];
 
 fn add(ins: Instruction) {
-
+    dbg!(ins);
 }
 fn sub(ins: Instruction) {
 
 }
 fn empty_fun(ins: Instruction) {
-    todo!()
+    dbg!(ins);
+    println!("Unsupported function ! {}", ins);
 }
+type InstructionDescription = (&'static str, InstructionFormat, InstructionMask, fn(Instruction));
+type _ReverseInstructionsMasks = [Vec<InstructionDescription>; 127];
+static mut REVERSE_INSTRUCTIONS_MASKS: OnceCell<_ReverseInstructionsMasks> = OnceCell::new();
+fn set_instructions_funcs() {
+    let mut instru_funcs: _ReverseInstructionsMasks = std::array::from_fn(|_| Vec::new());
+    for (i,(name, format, mask, fun)) in INSTRUCTIONS_MASKS.iter().enumerate() {
+        let opcode = Instruction(mask.0).opcode();
+        instru_funcs[opcode as usize].push((name, *format, *mask, *fun));
+    }
+    unsafe{REVERSE_INSTRUCTIONS_MASKS.set(instru_funcs).unwrap()}
+}
+
 
 fn exec_func(instruction: Instruction) {
-    let funcs: [LinkedList<(Instruction, fn(Instruction))>; 63] = std::array::from_fn(|_| LinkedList::new());
-
+    let instructions = &unsafe{REVERSE_INSTRUCTIONS_MASKS.get().unwrap()}[instruction.opcode() as usize];
+    let (_name, _fmt, _mask, fun) = find_instruction_desc(instruction);
+    fun(instruction);
+    // println!("Instruction not found \"{:?}\"", instruction);
 }
 
 
-// Elements are linked lists of a fun3 and fun7 and the str name
-fn get_ops() -> [LinkedList<(Instruction, &'static str)>; 128] {
-    const _L: LinkedList<(Instruction, &'static str)> = LinkedList::new();
-    let mut ops = [_L; 128];
-    let mut i = 0;
-    while i < INSTRUCTIONS_MASKS.len() { // Can't use for loop :c
-        let (name, format, op) = &INSTRUCTIONS_MASKS[i];
-        let inst = Instruction(*op);
-        let list = &mut ops[inst.opcode() as usize];
-        list.push_back((inst, name));
-        i+=1;
+
+fn get_from_opcode(opcode:u32) -> &'static Vec<InstructionDescription> {
+    unsafe{&REVERSE_INSTRUCTIONS_MASKS.get().unwrap()[opcode as usize]}
+}
+fn try_find_instruction_desc(inst: Instruction) -> Option<InstructionDescription> {
+    let opcode = inst.opcode();
+    let neighbors = unsafe{&REVERSE_INSTRUCTIONS_MASKS.get().unwrap()[opcode as usize]};
+    if neighbors.is_empty() {return None;}
+    let fmt = neighbors[0].1;
+    for (_name, _fmt, mask, fun) in neighbors {
+        let mi = Instruction(mask.0);
+        if match fmt {
+            InstructionFormat::R => {mi.fun3() == inst.fun3() && mi.fun7() == inst.fun7()},
+            InstructionFormat::I => {mi.fun3() == inst.fun3()},
+            InstructionFormat::S => {mi.fun3() == inst.fun3()},
+            InstructionFormat::B => {mi.fun3() == inst.fun3()},
+            InstructionFormat::U => {true},
+            InstructionFormat::J => {true},
+        } {
+            return Some((_name, fmt, *mask, *fun))
+        }
     }
-    ops
+    println!("{:?}", inst);
+    None
 }
-static mut OPS: OnceCell<[LinkedList<(Instruction, &'static str)>; 128]> = OnceCell::new();
-
-fn get_from_opcode(opcode:u32) -> &'static LinkedList<(Instruction, &'static str)> {
-    unsafe{&OPS.get().unwrap()[opcode as usize]}
+fn find_instruction_desc(inst: Instruction) -> InstructionDescription {
+    try_find_instruction_desc(inst).unwrap()
 }
 
 #[derive(Clone, Copy)]
 pub struct Instruction(pub u32);
 impl Instruction {
+    pub fn new(inst: u32) -> Option<Self> {
+        let s = Self(inst);
+        try_find_instruction_desc(s)?;
+        Some(s)
+    }
     // 7 bits
     pub fn opcode(self) -> u32 {
         self.0.get_bits(0..=6)
@@ -219,9 +251,7 @@ impl Instruction {
         self.0.get_bits(25..=31)
     }
     pub fn format(self) -> InstructionFormat {
-        match self.opcode() {
-            _ => todo!(),
-        }
+        find_instruction_desc(self).1
     }
     // Depends on format (see self.format and `InstructionFormat`)
     pub fn auto_imm(self) -> u32 {
@@ -235,13 +265,9 @@ impl Instruction {
         }
     }
     fn _opcode_name(self) -> &'static str {
-        for (mask, name) in get_from_opcode(self.opcode()) {
-            if mask.fun3() == self.fun3() && mask.fun7() == self.fun7() {
-                return name
-            }
-        }
-        println!("Unknown instruction: {:x}", self.0);
-        "unknown" // Could return option ?
+        find_instruction_desc(self).0
+        // println!("Unknown instruction: {:x}", self.0);
+        // "unknown" // Could return option ?
     }
     fn _reg_to_str(reg: u32) -> &'static str {
         ["zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", 
@@ -263,7 +289,32 @@ impl Instruction {
         // }
     }
 }
+#[derive(Clone, Copy, Debug)]
+pub struct InstructionMask(pub u32);
+impl InstructionMask {
+    // Returns true if the mask corresponds to the same instruction
+    // Like if opcode is same, fun3 and fun7 if there is one
+    pub fn is_mask(self, instruction: Instruction) -> bool {
+        let s = Instruction(self.0);
+        if instruction.opcode() == s.opcode() {
+            return match instruction.format() {
+                InstructionFormat::R => {s.fun3() == instruction.fun3() && s.fun7() == instruction.fun7()},
+                InstructionFormat::I => {s.fun3() == instruction.fun3()},
+                InstructionFormat::S => {s.fun3() == instruction.fun3()},
+                InstructionFormat::B => {s.fun3() == instruction.fun3()},
+                InstructionFormat::U => {true},
+                InstructionFormat::J => {true},
+            }
+        }
+        false
+    }
+}
 impl std::fmt::Debug for Instruction {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> { 
+        fmt.write_str(&format!("{:b} {:b} {:b} {:b}", self.opcode(), self.fun3(), self.fun7(), self.0))
+    }
+}
+impl std::fmt::Display for Instruction {
     fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> { 
         fmt.write_str(&format!("{} {} {} {}", self._opcode_name(), Self::_reg_to_str(self.rd()), Self::_reg_to_str(self.rs1()), Self::_reg_to_str(self.rs2())))
     }
@@ -271,11 +322,13 @@ impl std::fmt::Debug for Instruction {
 
 
 pub fn run(program: Vec<u8>) {
-    unsafe{OPS.set(get_ops()).unwrap()};
+    set_instructions_funcs();
     let vm = VM::new(program);
     for i in 0..vm.program.len()/2 {
-        let instruction = Instruction(vm.get_dword(i));
-        if instruction._opcode_name() == "unknown" {break;} // The file currently has some unknown instructions
-        println!("{:?}",instruction);
+        let instruction = Instruction::new(vm.get_dword(i));
+        if instruction.is_none() {break;} // The file currently has some unknown instructions
+        let instruction = instruction.unwrap();
+        println!("{}",instruction);
+        exec_func(instruction)
     }
 }
